a72c77aae6a5ae1a1a88c182fba8f08558487906
yang vieira <yangflix3@gmail.com>
2022-08-13 12:48:58 -0400

    :sparkles: feat class Chicken

--- src/farm-classes/Animals/Chicken.cs
+++ src/farm-classes/Animals/Chicken.cs
@@ -1,3 +1,19 @@
 namespace Animals;
 
-// Crie a sua classe Chicken aqui
+class Chicken {
+    public Guid Id =  Guid.NewGuid();
+    public int EggsPerWeek;
+    public Chicken? Mother;
+
+    public Chicken(int EggsPerWeek, Chicken Mother) {
+           if(EggsPerWeek < 0) {
+            throw new ArgumentException();
+           }
+
+
+        this.EggsPerWeek = EggsPerWeek;
+        this.Mother = Mother;
+    }
+    
+
+}
